ef1b986f54fac6076b042f8f0f8a1fd92688aa6b
sydney-pi <sydney.bang94@gmail.com>
2023-10-16 01:00:11 -0400

    organizing the numbers to print

--- Scripts/Boards.gd
+++ Scripts/Boards.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 var boardTileMap : TileMap
+var labelInstance: PackedScene = load("res://Scenes/LineLabel.tscn")
+
 var boardSize : Vector2 = Vector2(10, 10)
 var HValues: Array = []
 var VValues: Array = []
@@ -26,44 +28,51 @@ func generate_board():
 	for x in range(int(boardSize.x)):
 		for y in range(int(boardSize.y)):
 			boardTileMap.set_cell(0, Vector2i(x, y), map[y][x], Vector2i(0, 0), 0)
-	getColData()
 	getRowData()
+	getColData()
 	
-func getColData():
-	var chain = 0
+	printRowData()
 	
+func printRowData():
 	for y in range(int(boardSize.y)):
-		for x in range(int(boardSize.x)):
-			if boardTileMap.get_cell_source_id(0, Vector2i(x,y)) == 0:
-				if chain > 0:
-					HValues.append(chain)
-						
-					print("y: ", y, " = ", chain)
-					chain = 0
-
-			else :
-				chain += 1
-		if chain > 0:
-			HValues.append(chain)
-			print("y: ", y, " = ", chain)
-			chain = 0
-			HValues.append([])
+		for chain in HValues[y]:
+			print(chain)
 	
 func getRowData():
 	var chain = 0
-	HValues.append([])	
-	for x in range(int(boardSize.x)):
-		for y in range(int(boardSize.y)):
+	for y in range(int(boardSize.y)):
+		HValues.append([])
+		for x in range(int(boardSize.x)):
+			
 			if boardTileMap.get_cell_source_id(0, Vector2i(x,y)) == 0:
 				if chain > 0:
-					HValues.append(chain)
-					print("x: ", x, " = ", chain)
+					HValues[y].append(chain)
+						
+					#print("y: ", y, " = ", chain)
 					chain = 0
-					HValues.append([])
 			else :
 				chain += 1
 		if chain > 0:
-			HValues.append(chain)
-			print("x: ", x, " = ", chain)
+			HValues[y].append(chain)
+			#print("y: ", y, " = ", chain)
 			chain = 0
-			HValues.append([])
+		
+	print("Hvalues, ", HValues)
+	
+func getColData():
+	var chain = 0
+	for x in range(int(boardSize.x)):
+		VValues.append([])
+		for y in range(int(boardSize.y)):
+			if boardTileMap.get_cell_source_id(0, Vector2i(x,y)) == 0:
+				if chain > 0:
+					VValues[x].append(chain)
+					#print("x: ", x, " = ", chain)
+					chain = 0
+			else :
+				chain += 1
+		if chain > 0:
+			VValues[x].append(chain)
+			#print("x: ", x, " = ", chain)
+			chain = 0
+	print("Vvalues, ", VValues)
